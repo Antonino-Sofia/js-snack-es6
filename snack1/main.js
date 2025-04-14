22ad8e38ad0d230e0b85b2592d920c00ed65ca48
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-14 17:24:48 +0200

    js-snacks-es6

---
+++ snack1/main.js
@@ -0,0 +1,31 @@
+console.log("Snack 1");
+
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+const bikes = [
+  {
+    nome: "Bike 1",
+    peso: 7, //peso inferiore: 7
+  },
+  {
+    nome: "Bike wow",
+    peso: 5, //peso inferiore 5
+  },
+  {
+    nome: "Bike 2",
+    peso: 14, //peso inferiore: 7
+  },
+  {
+    nome: "Bike 4",
+    peso: 20, //peso inferiore 5
+  },
+  {
+    nome: "Bike 5",
+    peso: 5, //peso inferiore: 7
+  },
+];
